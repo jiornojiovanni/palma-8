79872d9ae0075c4e0742e5635605ef6c01e493f0
jiornojiovanni <gianni00palmieri@gmail.com>
2021-05-27 10:44:45 +0200

    implemented collision detection during draw

--- src/interpreter.c
+++ src/interpreter.c
@@ -52,6 +52,7 @@ void cicle(VM *state)
         break;
     case 0x0D: //DXYN Draws a sprite at coordinate (VX, VY) with height N
         height = opcode & 0x000F;
+        int collisionDetected = 0;
         for (int i = 0; i < height; i++)
         {
             char row = state->RAM[state->I + i] >> 4;
@@ -64,12 +65,30 @@ void cicle(VM *state)
                 {
                     int vX = (opcode & 0x0F00) >> 8;
                     int vY = (opcode & 0x00F0) >> 4;
+                    if (state->video[state->V[vX] + i][state->V[vY] + j] == '*')
+                    {
+                        state->video[state->V[vX] + i][state->V[vY] + j] = 0;
+                        collisionDetected = 1;
+                    }
+                    else
+                    {
                     state->video[state->V[vX] + i][state->V[vY] + j] = '*';
+                    }
                 }
                 j++;
                 bit--;
             } while (bit > -1);
         }
+
+        if (collisionDetected)
+        {
+            state->V[0xF] = 1;
+        }
+        else
+        {
+            state->V[0xF] = 0;
+        }
+
         state->PC += 2;
         break;
     case 0x01: //1NNN Jumps to address NNN.
