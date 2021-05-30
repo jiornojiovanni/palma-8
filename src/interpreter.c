ed6291cca25ad22e26d176c3fcadd75581bd3f62
jiornojiovanni <gianni00palmieri@gmail.com>
2021-05-30 17:33:02 +0200

    changed terminology

--- src/interpreter.c
+++ src/interpreter.c
@@ -43,16 +43,16 @@ void halt(unsigned char instruction)
 
 void cicle(VM *state)
 {
-    unsigned char instruction, nextInstruction, height;
-    unsigned short opcode;
-    instruction = state->RAM[state->PC + OFFSET];
-    nextInstruction = state->RAM[state->PC + OFFSET + 1];
-    opcode = (instruction << 8) | nextInstruction;
+    unsigned char opcode, nextOpcode, height;
+    unsigned short instruction;
+    opcode = state->RAM[state->PC + OFFSET];
+    nextOpcode = state->RAM[state->PC + OFFSET + 1];
+    instruction = (opcode << 8) | nextOpcode;
 
-    switch (instruction >> 4)
+    switch (opcode >> 4)
     {
     case 0x00:
-        switch (nextInstruction)
+        switch (nextOpcode)
         {
         case 0xE0: //00E0 Clears the screen.
             memset(state->video, 0, 64 * 32);
@@ -66,124 +66,124 @@ void cicle(VM *state)
             break;
             
         default:
-            halt(nextInstruction);
+            halt(nextOpcode);
             break;
         }
         break;
 
     case 0x01: //1NNN Jumps to address NNN.
-        state->PC = (opcode & 0x0FFF) - OFFSET;
+        state->PC = (instruction & 0x0FFF) - OFFSET;
         break;
 
     case 0x02: //2NNN Call the subroutine at NNN.
         state->STACK[state->SP] = state->PC;
         state->SP++;
-        state->PC = (opcode & 0x0FFF) - OFFSET;
+        state->PC = (instruction & 0x0FFF) - OFFSET;
         break;
 
     case 0x03: //3XNN Skips the next instruction if VX equals NN.
-        state->PC += state->V[instruction & 0x0F] == nextInstruction ? 4 : 2;
+        state->PC += state->V[opcode & 0x0F] == nextOpcode ? 4 : 2;
         break;
 
     case 0x04: //4XNN Skips the next instruction if VX does not equal NN.
-        state->PC += state->V[instruction & 0x0F] != nextInstruction ? 4 : 2;
+        state->PC += state->V[opcode & 0x0F] != nextOpcode ? 4 : 2;
         break;
 
     case 0x05: //5XY0 Skips the next instruction if VX equals VY.
-        if((nextInstruction & 0x0F) != 0) halt(nextInstruction);
-        state->PC += state->V[instruction & 0x0F] == state->V[nextInstruction >> 4] ? 4 : 2;
+        if((nextOpcode & 0x0F) != 0) halt(nextOpcode);
+        state->PC += state->V[opcode & 0x0F] == state->V[nextOpcode >> 4] ? 4 : 2;
         break;
 
     case 0x06: //6XNN Sets VX to NN.
-        state->V[instruction & 0x0F] = opcode & 0x00FF;
+        state->V[opcode & 0x0F] = instruction & 0x00FF;
         state->PC += 2;
         break;
 
     case 0x07: //7XNN Add NN to Vx (without carry flag change).
-        state->V[instruction & 0x0F] += opcode & 0x00FF;
+        state->V[opcode & 0x0F] += instruction & 0x00FF;
         state->PC += 2;
         break;
 
     case 0x08:
-        switch (nextInstruction & 0x0F)
+        switch (nextOpcode & 0x0F)
         {
         case 0x00: //8XY0 Sets VX to the value of VY.
-            state->V[instruction & 0x0F] = state->V[nextInstruction >> 4];
+            state->V[opcode & 0x0F] = state->V[nextOpcode >> 4];
             state->PC += 2;
             break;
 
         case 0x01: //8XY1 Sets VX to VX or VY.
-            state->V[instruction & 0x0F] = state->V[instruction & 0x0F] | state->V[nextInstruction >> 4];
+            state->V[opcode & 0x0F] = state->V[opcode & 0x0F] | state->V[nextOpcode >> 4];
             state->PC += 2;
             break;
 
         case 0x02: //8XY2 Sets VX to VX and VY.
-            state->V[instruction & 0x0F] = state->V[instruction & 0x0F] & state->V[nextInstruction >> 4];
+            state->V[opcode & 0x0F] = state->V[opcode & 0x0F] & state->V[nextOpcode >> 4];
             state->PC += 2;
             break;
 
         case 0x03: //8XY3 Sets VX to VX xor VY.
-            state->V[instruction & 0x0F] = state->V[instruction & 0x0F] ^ state->V[nextInstruction >> 4];
+            state->V[opcode & 0x0F] = state->V[opcode & 0x0F] ^ state->V[nextOpcode >> 4];
             state->PC += 2;
             break;
 
         case 0x04: //8XY4 Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there is not.
-            state->V[instruction & 0x0F] += state->V[nextInstruction >> 4];
-            state->V[0xF] = state->V[instruction & 0x0F] + state->V[nextInstruction >> 4] > 255 ? 1 : 0;
+            state->V[opcode & 0x0F] += state->V[nextOpcode >> 4];
+            state->V[0xF] = state->V[opcode & 0x0F] + state->V[nextOpcode >> 4] > 255 ? 1 : 0;
             state->PC += 2;
             break;
 
         case 0x05: //8XY5 VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there is not.
-            state->V[0xF] = state->V[instruction & 0x0F] > state->V[nextInstruction >> 4] ? 1 : 0;
-            state->V[instruction & 0x0F] -= state->V[nextInstruction >> 4];
+            state->V[0xF] = state->V[opcode & 0x0F] > state->V[nextOpcode >> 4] ? 1 : 0;
+            state->V[opcode & 0x0F] -= state->V[nextOpcode >> 4];
             state->PC += 2;
             break;
 
         case 0x06: //8XY6 Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
-            state->V[0xF] = state->V[instruction & 0x0F] & 1;
-            state->V[instruction & 0x0F] = state->V[instruction & 0x0F] >> 1;
+            state->V[0xF] = state->V[opcode & 0x0F] & 1;
+            state->V[opcode & 0x0F] = state->V[opcode & 0x0F] >> 1;
             state->PC += 2;
             break;
 
         case 0x07: //8XY7 Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there is not.
-            state->V[0xF] = state->V[nextInstruction >> 4] > state->V[instruction & 0x0F] ? 1 : 0;
-            state->V[nextInstruction >> 4] -= state->V[instruction & 0x0F];
+            state->V[0xF] = state->V[nextOpcode >> 4] > state->V[opcode & 0x0F] ? 1 : 0;
+            state->V[nextOpcode >> 4] -= state->V[opcode & 0x0F];
             state->PC += 2;
             break;
 
         case 0x0E: //8XYE Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
-            state->V[0xF] = state->V[instruction & 0x0F] >> 3;
-            state->V[instruction & 0x0F] = state->V[instruction & 0x0F] << 1;
+            state->V[0xF] = state->V[opcode & 0x0F] >> 3;
+            state->V[opcode & 0x0F] = state->V[opcode & 0x0F] << 1;
             state->PC += 2;
             break;
 
         default:
-            halt(nextInstruction);
+            halt(nextOpcode);
             break;
         }
         break;
 
     case 0x09: //9XY0 Skips the next instruction if VX does not equal VY. (Usually the next instruction is a jump to skip a code block).
-        if((nextInstruction & 0x0F) != 0) halt(nextInstruction);
-        state->PC += state->V[instruction & 0x0F] != state->V[nextInstruction >> 4] ? 4 : 2;
+        if((nextOpcode & 0x0F) != 0) halt(nextOpcode);
+        state->PC += state->V[opcode & 0x0F] != state->V[nextOpcode >> 4] ? 4 : 2;
         break;
 
     case 0x0A: //ANNN Sets I to the address NNN.
-        state->I = opcode & 0x0FFF;
+        state->I = instruction & 0x0FFF;
         state->PC += 2;
         break;
 
     case 0x0B: //BNNN Jumps to the address NNN plus V0.
-        state->PC = ((opcode & 0x0FFF) + state->V[0x00]) - OFFSET;
+        state->PC = ((instruction & 0x0FFF) + state->V[0x00]) - OFFSET;
         break;
 
     case 0x0C: //CXNN Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
-        state->V[instruction & 0x0F] = (rand() % 256) & nextInstruction;
+        state->V[opcode & 0x0F] = (rand() % 256) & nextOpcode;
         state->PC += 2;
         break;
 
     case 0x0D: //DXYN Draws a sprite at coordinate (VX, VY) with height N.
-        height = opcode & 0x000F;
+        height = instruction & 0x000F;
         int collisionDetected = 0;
         for (int i = 0; i < height; i++)
         {
@@ -196,8 +196,8 @@ void cicle(VM *state)
             {
                 if ((row >> bit) & 0x01)
                 {
-                    int vX = (opcode & 0x0F00) >> 8;
-                    int vY = (opcode & 0x00F0) >> 4;
+                    int vX = (instruction & 0x0F00) >> 8;
+                    int vY = (instruction & 0x00F0) >> 4;
                     unsigned char y = (state->V[vY] + i) % (ROWS - 1);
                     unsigned char x = (state->V[vX] + j) % (COLUMNS - 1);
                     if (state->video[y][x] == 1)
@@ -221,7 +221,7 @@ void cicle(VM *state)
         break;
 
     default:
-        halt(instruction);
+        halt(opcode);
         break;
     }
 }
